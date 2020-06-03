9f16c45adf7b17935fefd09cd42c7bf1d69cc569
JmDeleon2000 <dele19817@uvg.edu.gt>
2020-06-02 22:07:34 -0600

    Added challenge creation screen UI

---
+++ app/src/main/java/com/example/risegrindgobeyond/Challenge_Creation.kt
@@ -0,0 +1,12 @@
+package com.example.risegrindgobeyond
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Challenge_Creation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge__creation)
+    }
+}
